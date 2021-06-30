43d3cbc788b604af700145de0701fc31bb369ad4
Iosismu <evanpark333@gmail.com>
2021-06-30 20:51:04 +0900

    'test1'

--- js/main.js
+++ js/main.js
@@ -111,7 +111,7 @@ spyEls.forEach(function (spyEl) {
   new ScrollMagic.Scene({
     // 감시할 장면(Scene)을 추가
     triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-    triggerHook: 0.4, // 화면의 80% 지점에서 보여짐 여부 감시
+    triggerHook: 0.8, // 화면의 80% 지점에서 보여짐 여부 감시
   })
     .setClassToggle(spyEl, "show") // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
